b0b60487cdd43072abbe4e28172953b8e5745be6
cagdas <cagdaskorkut@gmail.com>
2017-01-16 15:35:16 +0300

    register sayfasını koydum. çalıştırmadım.

---
+++ WebUI/Models/Account/CreateUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models.Account
+{
+    public class CreateUserModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage ="Olmaz")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
